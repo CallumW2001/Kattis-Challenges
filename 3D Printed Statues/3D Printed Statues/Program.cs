1a4c09c15763eaa69e6f4e652e91330c946d3f8a
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2025-02-09 11:58:15 +0000

    Add files via upload

---
+++ 3D Printed Statues/3D Printed Statues/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Runtime.Serialization;
+
+
+int statuesToPrint = int.Parse(Console.ReadLine());
+int numberOfPrinters = 1;
+int days = 0;
+if(statuesToPrint == 1)
+{
+    days = days + 1;
+    Console.WriteLine(days);
+}
+
+else if(statuesToPrint > 0)
+{
+
+    while(numberOfPrinters < statuesToPrint)
+    {
+        numberOfPrinters = numberOfPrinters * 2;
+        days = days + 1;
+    }
+
+    days = days + 1;
+
+    Console.WriteLine(days);
+}
