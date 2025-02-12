6b16868d6459da69aef6d9b0d1c02406250ec861
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2025-02-12 13:50:25 +0000

    Add files via upload

---
+++ A Cappella Recording/A Cappella Recording/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+int[] inputs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int numOfNotes = inputs[0];
+int largestDifference = inputs[1];
+
+List<int> pitches = new List<int>();
+
+for(int i = 0; i < numOfNotes; i++)
+{
+    pitches.Add(int.Parse(Console.ReadLine()));
+}
+
+pitches.Sort();
+
+int recordings = 1;
+int currentNote = pitches[0];
+
+for (int i = 1; i < pitches.Count; i++)
+{
+    if (pitches[i] > currentNote + largestDifference && pitches[i] != currentNote)
+    {
+        recordings++;
+        currentNote = pitches[i];
+    }
+    
+}
+
+Console.WriteLine(recordings);
