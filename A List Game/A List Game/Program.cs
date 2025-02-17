3e5afb222fa8bf27dd6b2fb5a42375dd94b1d5b2
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2025-02-17 10:34:52 +0000

    Add files via upload

---
+++ A List Game/A List Game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+int firstPlayer = int.Parse(Console.ReadLine());
+int count = 0;
+int factor = 2;
+
+
+
+while (factor * factor <= firstPlayer)
+{
+    if (firstPlayer % factor == 0)
+    {
+
+        firstPlayer /= factor;
+        count++;
+
+    }
+    else factor++;
+}
+
+count++;
+Console.WriteLine(count);
