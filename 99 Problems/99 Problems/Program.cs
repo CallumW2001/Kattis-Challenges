f79c5935afadfa8b922acf3a0438c782f5178a44
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2025-02-18 10:04:35 +0000

    Add files via upload

---
+++ 99 Problems/99 Problems/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Collections.Generic;
+
+int n = int.Parse(Console.ReadLine());
+
+int last = Last2Digits(n);
+
+    if (n <= 99)
+    {
+        Console.WriteLine("99");
+    }
+    else
+    {
+
+        if (last < 49)
+        {
+            Console.WriteLine(n - (last) - 1);
+        }
+        else
+        {
+            Console.WriteLine(n + 100 - (last) - 1);
+        }
+
+    }
+static int Last2Digits(int n)
+{
+
+    string num = n.ToString();
+    string first;
+    string second;
+    int Last2Digits;
+
+    List<char> digits = num.ToList();
+
+    if (n >= 10)
+    {
+        first = digits[^2].ToString();
+        second = digits[^1].ToString();
+        Last2Digits = int.Parse(first + second);
+        return Last2Digits;
+    }
+    else
+    {
+        return int.Parse(digits[^1].ToString());
+    }
+}
