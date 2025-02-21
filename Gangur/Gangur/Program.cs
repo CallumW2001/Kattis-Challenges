9eb135a32e76e74602c2a6193a57372c616af67e
CallumW2001 <46678107+CallumW2001@users.noreply.github.com>
2025-02-21 09:16:09 +0000

    Add files via upload

---
+++ Gangur/Gangur/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+string input = Console.ReadLine();
+List<char> movements = input.ToList();
+
+
+
+int pass = 0;
+
+for(int i = 0; i < movements.Count; i++)
+{
+
+    if (movements[i] == '>')
+    {
+        for(int j = i; j < movements.Count; j++)
+        {
+            if (movements[j] == '<')
+            {
+                pass++;
+            }
+        }
+    }
+
+}
+
+Console.WriteLine(pass);
